3730d31caf536eb898e528ebfa528cb02913f61f
Zetaphor <zetaphor@hey.com>
2022-09-03 18:24:12 -0500

    Mostly working

--- engine.js
+++ engine.js
@@ -3,41 +3,114 @@ const fixedTimeStep = 1.0 / 60.0; // seconds
 const maxSubSteps = 3;
 let runSimulation = false;
 
-var sphereBody, groundBody;
+const bodyTypes = {
+  1: 'sphere',
+  2: 'plane',
+  4: 'box',
+  16: 'cylinder',
+};
 
 const rotationVec = CANNON.Vec3();
 
-function initWorld() {
-  world = new CANNON.World();
-  world.gravity.set(0, 0, -9.82); // m/s²
-}
-
-function startWorld() {
+function buildWorld() {
+  initWorld();
+  createPlane();
   runSimulation = true;
   update();
 }
 
+function initWorld() {
+  world = new CANNON.World();
+  world.gravity.set(0, 0, -9.82); // m/s²
+  updateSimulationStatus("Running");
+}
+
+function resumeWorld() {
+  lastTime = 0;
+  runSimulation = true;
+  updateSimulationStatus("Running");
+}
+
 function stopWorld() {
   runSimulation = false;
+  lastTime = 0;
+  updateSimulationStatus("Paused");
 }
 
-function createSphere() {
-  const radius = 1; // m
-  sphereBody = new CANNON.Body({
-    mass: 5, // kg
-    position: new CANNON.Vec3(0, 0, 10), // m
+function resetWorld() {
+  for (var i = 0; i < world.bodies.length; i++) {
+    const body = world.bodies[i];
+    body.position.copy(body.initPosition);
+    body.velocity.copy(body.initVelocity);
+    if (body.initAngularVelocity) {
+      body.angularVelocity.copy(body.initAngularVelocity);
+      body.quaternion.copy(body.initQuaternion);
+    }
+  }
+}
+
+function createBody(type, mass, position, rotation, scale) {
+  if (type === 'box') createBox(mass, position, rotation, scale);
+  else if (type === 'sphere') createSphere(mass, position, rotation, scale);
+  else if (type === 'cylinder') createCylinder(mass, position, rotation, scale);
+}
+
+function removeBody(id) {
+  for (let i = 0; i < world.bodies.length; i++) {
+    const body = world.bodies[i];
+    if (world.bodies[i].id === id) {
+      world.remove(body);
+      break;
+    }
+  }
+}
+
+function createSphere(mass, position, rotation, radius) {
+  let sphereBody = new CANNON.Body({
+    mass: mass, // kg
+    position: position, // m
     shape: new CANNON.Sphere(radius),
+    quaternion: rotation
   });
   world.addBody(sphereBody);
+  console.log('Create Sphere:', sphereBody);   
 }
 
-function createGround() {
-  groundBody = new CANNON.Body({
-    mass: 0, // mass == 0 makes the body static
+function createBox(mass, position, rotation, scale) {
+  let boxBody = new CANNON.Body({
+    mass: mass,
+    shape: new CANNON.Box(scale),
+    position: position,
+    quaternion: rotation
   });
-  let groundShape = new CANNON.Plane();
-  groundBody.addShape(groundShape);
+  world.addBody(boxBody);
+  console.log('Create Box:', boxBody); 
+}
+
+function createCylinder(mass, position, rotation, scale) {
+  console.log(mass, position, rotation, scale);
+  let cylinderBody = new CANNON.Body({
+    mass: mass,
+    shape: new CANNON.Cylinder(scale, scale, scale * 2.2, 10),
+    position: position,
+    quaternion: rotation
+  });
+  world.addBody(cylinderBody);
+  console.log('Create Cylinder:', cylinderBody); 
+}
+
+function createPlane() {
+  let groundBody = new CANNON.Body({
+    mass: 0, // mass == 0 makes the body static
+    shape: new CANNON.Plane(),
+  });
+  groundBody.position.set(0, 0, 0);
   world.addBody(groundBody);
+  console.log('Create Plane:', groundBody);   
+}
+
+function removeBody(id) {
+  world.remove()
 }
 
 function update(time) {
@@ -49,9 +122,11 @@ function update(time) {
   }
 
   for (let i = 0; i < world.bodies.length; i++) {
-    const element = world.bodies[i];
-    sendPhysicsUpdate(world.bodies[i].id, world.bodies[i].position, world.bodies[i].quaternion);
+    const body = world.bodies[i];
+    if (body.shapes[0].type === 2) continue; // Don't send updates for the ground plane
+    sendPhysicsUpdate(body.id, bodyTypes[body.shapes[0].type], body.position, body.quaternion);
   }
+  updateSimulationBodyCount(world.bodies.length);
 
   lastTime = time;
   requestAnimationFrame(update);
--- websockets.js
+++ websockets.js
@@ -1,7 +1,4 @@
-// var url = "ws://localhost:8080/echo";
-// var url = "ws://localhost:8080/chat";
 var url = "ws://localhost:8080/chat-echo";
-// var url = "ws://localhost:8080/chat-id";
 var output;
 var socketReady = false;
 
@@ -14,21 +11,26 @@ function init() {
 
   websocket.onopen = function (e) {
     socketReady = true;
-    initWorld();
-    createSphere();
-    createGround();
-    startWorld();
     document.getElementById("websocketStatus").innerHTML = "Connected";
+    document.getElementById("websocketOutputContainer").style.display = "block";    
+    document.getElementById("simulationControls").style.display = "block";
+    document.getElementById("simluationStatusContainer").style.display = "block";
+    buildWorld();
   };
 
   websocket.onmessage = function (e) {
-    document.getElementById("websocketOutput").innerHTML = e.data;
+    if (e.data === "pause") stopWorld();
+    else if (e.data === "resume") resumeWorld();    
+    else if (e.data === "reset") resetWorld();
+    else if (e.data.startsWith("add")) addBodyFromString(e.data);
+    else if (e.data.startsWith("remove")) removeBodyFromString(e.data);
+    else document.getElementById("websocketOutput").innerHTML = e.data;
   };
 
   websocket.onerror = function (e) {
     socketReady = false;
     stopWorld();
-    document.getElementById("websocketStatus").innerHTML = '<span style="color: red;">ERROR: ' + e.data + "</span>";
+    document.getElementById("websocketError").style.display = "block";
   };
 
   websocket.onclose = function (e) {
@@ -38,11 +40,47 @@ function init() {
   };
 }
 
-function sendPhysicsUpdate(id, position, rotation) {
+function updateSimulationStatus(status) {
+  document.getElementById("simulationStatus").innerHTML = status;
+}
+
+function updateSimulationBodyCount(totalBodies) {
+  document.getElementById("simulationTotalBodies").innerHTML = totalBodies;  
+}
+
+function sendPhysicsUpdate(id, bodyType, position, rotation) {
   if (!socketReady) return;
   websocket.send(
-    `${id}|${position.x},${position.y},${position.z}|${rotation.w},${rotation.x},${rotation.y},${rotation.z}`
+    `${id}#${bodyType}%[${position.x};${position.y};${position.z}]|[${rotation.w};${rotation.x};${rotation.y};${rotation.z}]`
   );
+}
+
+function addBodyFromString(input) {
+  // addBox|mass|position|rotation|scale
+  inputArgs = input.split('|');
+  const bodyType = inputArgs[0].split('add')[1].toLowerCase();
+  const mass = parseFloat(inputArgs[1]);
+
+  const positionArgs = inputArgs[2].split(',');
+  const position = new CANNON.Vec3(parseFloat(positionArgs[0]), parseFloat(positionArgs[1]), parseFloat(positionArgs[2]));
+
+  const rotationArgs = inputArgs[3].split(',');
+  const rotation = new CANNON.Quaternion(parseFloat(rotationArgs[0]), parseFloat(rotationArgs[1]), parseFloat(rotationArgs[2]), parseFloat(rotationArgs[3]));
+
+  let scale = 0;
+  console.log(inputArgs);
+  if (bodyType === 'cylinder' || bodyType === 'sphere') scale = parseFloat(inputArgs[4]);
+  else {
+    const scaleArgs = inputArgs[4].split(',');
+    scale = new CANNON.Vec3(parseFloat(scaleArgs[0]), parseFloat(scaleArgs[1]), parseFloat(scaleArgs[2]));
+  }
+  
+  createBody(bodyType, mass, position, rotation, scale); 
+}
+
+function removeBodyFromString(input) {
+  removeBody(parseFloat(input.split('|')[1]));
+  console.log('removeBody', input);
 }
 
 window.addEventListener("load", init, false);
