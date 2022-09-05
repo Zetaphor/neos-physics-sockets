88ae724a2535b72f7a5edf8a9936fe62e8c3d677
Zetaphor <zetaphor@hey.com>
2022-09-04 22:32:27 -0500

    Update websocket create

--- websockets.js
+++ websockets.js
@@ -1,10 +1,10 @@
-class NeosPhysicsSockets {
+class NeosPhysicsSockets extends EventTarget {
   url = "ws://localhost:8080/chat-echo";
   output;
   socketReady = false;
   websocket = null;
-
   constructor() {
+    super();
     this.output = document.getElementById("output");
     this.websocket = new WebSocket(this.url);
     this.websocket.onopen = this.socketOpen;
@@ -17,31 +17,33 @@ class NeosPhysicsSockets {
     this.socketReady = true;
     document.getElementById("websocketStatus").innerHTML = "Connected";
     document.getElementById("websocketOutputContainer").style.display = "block";
-    document.getElementById("simulationControls").style.display = "block";
-    document.getElementById("simluationStatusContainer").style.display = "block";
     buildWorld();
   };
 
   socketError = (ev) => {
     this.socketReady = false;
-    stopWorld();
+    this.stopWorld();
     document.getElementById("websocketError").style.display = "block";
   };
 
   socketClose = (ev) => {
     this.socketReady = false;
-    stopWorld();
+    this.stopWorld();
     document.getElementById("websocketStatus").innerHTML = "Disconnected";
   };
 
   socketMessage = (ev) => {
-    if (ev.data === "pause") stopWorld();
+    if (ev.data === "pause") this.stopWorld();
     else if (ev.data === "resume") resumeWorld();
     else if (ev.data === "reset") resetWorld();
     else if (ev.data.startsWith("add")) addBodyFromString(ev.data);
     else if (ev.data.startsWith("remove")) removeBodyFromString(ev.data);
     else document.getElementById("websocketOutput").innerHTML = ev.data;
   };
+
+  stopWorld() {
+    dispatchEvent(new Event("stop"));
+  }
 
   updateSimulationStatus = (status) => {
     document.getElementById("simulationStatus").innerHTML = status;
@@ -63,6 +65,10 @@ class NeosPhysicsSockets {
     this.websocket.send("reset");
   };
 
+  receivedWorldReset = () => {
+    dispatchEvent(new Event("reset"));
+  };
+
   addBodyFromString = (input) => {
     // addBox|mass|position|rotation|scale
     inputArgs = input.split("|");
@@ -70,29 +76,31 @@ class NeosPhysicsSockets {
     const mass = parseFloat(inputArgs[1]);
 
     const positionArgs = inputArgs[2].split(",");
-    const position = new CANNON.Vec3(
-      parseFloat(positionArgs[0]),
-      parseFloat(positionArgs[1]),
-      parseFloat(positionArgs[2])
-    );
+    const position = { x: parseFloat(positionArgs[0]), y: parseFloat(positionArgs[1]), z: parseFloat(positionArgs[2]) };
 
     const rotationArgs = inputArgs[3].split(",");
-    const rotation = new CANNON.Quaternion(
-      parseFloat(rotationArgs[0]),
-      parseFloat(rotationArgs[1]),
-      parseFloat(rotationArgs[2]),
-      parseFloat(rotationArgs[3])
-    );
+    const rotation = {
+      x: parseFloat(rotationArgs[0]),
+      y: parseFloat(rotationArgs[1]),
+      z: parseFloat(rotationArgs[2]),
+      w: parseFloat(rotationArgs[3]),
+    };
 
     let scale = 0;
     console.log(inputArgs);
     if (bodyType === "cylinder" || bodyType === "sphere") scale = parseFloat(inputArgs[4]);
     else {
       const scaleArgs = inputArgs[4].split(",");
-      scale = new CANNON.Vec3(parseFloat(scaleArgs[0]), parseFloat(scaleArgs[1]), parseFloat(scaleArgs[2]));
+      scale = { x: parseFloat(positionArgs[0]), y: parseFloat(positionArgs[1]), z: parseFloat(positionArgs[2]) };
     }
 
-    createBody(bodyType, mass, position, rotation, scale);
+    dispatchEvent(new Event("createBody"), {
+      type: bodyType,
+      mass: mass,
+      position: position,
+      rotation: rotation,
+      scale: scale,
+    });
   };
 
   offlineStart = () => {
