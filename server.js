d543b9984565f64e604a8e501bf7ae8332ec44ba
Zetaphor <zetaphor@hey.com>
2022-09-21 21:11:28 -0500

    Rename server

--- server.js
+++ server.js
@@ -1,26 +1,57 @@
-// npm install uNetworking/uWebSockets.js#v16.2.0
-const uWS = require("uWebSockets.js");
+const Koa = require("koa");
+const http = require("http");
+const WebSocket = require("ws");
+const url = require("url");
 
-// an "app" is much like Express.js apps with URL routes,
-// here we handle WebSocket traffic on the wildcard "/*" route
-const app = uWS.App().ws("/*", {
-  // handle messages from client
+const app = new Koa();
+let masterWebsocket = null;
 
-  open: (socket, req) => {
-    /* For now we only have one canvas */
-    socket.subscribe("drawing/canvas1");
-  },
-  message: (socket, message, isBinary) => {
-    console.log(socket, message, isBinary);
-    /* In this simplified example we only have drawing commands */
-    app.publish("drawing/canvas1", message, true);
-    // socket.subscribe("test");
-  },
-});
-
-// finally listen using the app on port 9001
-app.listen(9001, (listenSocket) => {
-  if (listenSocket) {
-    console.log("Listening to port 9001");
+app.use((ctx) => {
+  ctx.set("Access-Control-Allow-Origin", "*");
+  if (ctx.url.startsWith("/init")) {
+    ctx.body = `success`;
+  } else {
+    ctx.body = "error";
   }
 });
+
+const wss = new WebSocket.Server({ noServer: true, clientTracking: false });
+
+const server = http.createServer(app.callback());
+
+server.on("upgrade", function upgrade(request, socket, head) {
+  const pathname = url.parse(request.url).pathname;
+
+  wss.on("connection", function connction(ws) {
+    console.log("Socket connection:", ws.clientId);
+  });
+
+  wss.handleUpgrade(request, socket, head, function done(ws) {
+    if (pathname === "/init") {
+      ws.clientId = "master";
+      ws.customData = {};
+      console.log("Initialized master socket");
+      ws.send("Initialized master socket");
+      masterWebsocket = ws;
+    } else if (pathname.startsWith("/createBody/")) {
+      // createBody/id/bodyType
+      pathParts = pathname.split("/");
+      ws.clientId = pathParts[2];
+      ws.customData = {
+        type: pathParts[3],
+        lastData: "",
+      };
+      masterWebsocket.send(`Created body #${pathParts[2]} of type ${pathParts[3]}`);
+      console.log(`Created body #${pathParts[2]} of type ${pathParts[3]}`);
+
+      // ws.on("message", function (message) {
+      //   if (ws.clientId === "master") {
+      //     console.log("Master client update", message);
+      //   }
+      // });
+    }
+  });
+});
+
+console.log("Server listening");
+server.listen(3000);
